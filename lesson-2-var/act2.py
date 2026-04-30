25622234e287e8627b3162c5718bdf07cb7e4b6b
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-04-30 14:03:13 +0530

    updates

---
+++ lesson-2-var/act2.py
@@ -0,0 +1,8 @@
+name ="Kevin Sanjeev"
+print("{} is attending {}".format(name,"codingal"))
+x=5 
+y=6
+z=x+y
+print("{}+{}={}".format(x ,y ,z))
+data = input("Enter your age :")
+print("You are{} years old".format(data))

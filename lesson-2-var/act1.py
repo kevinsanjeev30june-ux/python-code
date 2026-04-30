878586bb6ec0a146a5bc22e85a04195069410492
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-04-30 13:34:55 +0530

    first commit

---
+++ lesson-2-var/act1.py
@@ -0,0 +1,8 @@
+print("welcome to the world of python programming")
+print(4)
+print("hello",end="")
+print("world")
+print("Hello world \n")
+print("hello python",18) 
+print("welcome to ",end="*")
+      

81615cdc2acabbc723b73ebb17228561052c6b87
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-05 13:54:35 +0530

    updates

---
+++ lesson-4-operators/act4.py
@@ -0,0 +1,9 @@
+x = int(input("Enter value of first operand"))
+y = int(input("Enter value of second operand"))
+
+print("Is x equals to y ?", x==y)
+print("Is x not equals to y ?", x!=y)
+print("Is x greater than y ?", x>y)
+print("Is x lesser than y ?", x<y)
+print("Is x greater than or equal to  y ?", x>=y)
+print("Is x lesser than or equal to y ?", x<=y)

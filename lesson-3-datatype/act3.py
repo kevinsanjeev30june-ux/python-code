4f1a742e6795b75d1d562f74b11be66d7cfc2260
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-05 13:18:47 +0530

    updates

--- lesson-3-datatype/act3.py
+++
@@ -1,4 +0,0 @@
-text = input("Enter a String : ")
-print(text)
-revtext = text[::-1]
-print("Reverse of String is ",revtext)
---
+++ lesson-4-operators/act1.py
@@ -0,0 +1,14 @@
+# Scores in papers
+math_score = 99
+eng_score = 76
+social_score = 78
+science_score = 95
+tamil_score = 86
+
+# calculate and display the total score
+total_score = math_score + eng_score +social_score + science_score +tamil_score
+print("Total score in all 5 subjects:", total_score)
+
+# lets find the average
+average = total_score / 5
+print("Average of all the 5 scores is:", average)

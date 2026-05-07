09009362062e4492215df06a1f6fcd1583c41d4d
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-07 13:19:48 +0530

    updates

---
+++ lesson-6-logical operators/act1.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0 
+
+if ((a == 10) and(b == 12)):
+    print("It is an awesome day")
+else:
+    print("It is a horrible day")
+
+x = 8
+y = -10
+z = 6
+if x > 10 or y < 10:
+    print("All is well ")
+else:
+    print("Are you fine")  
+
+if c > 0 or z > 5:
+    print(" I love python")
+else:
+    print("I dont like python")    

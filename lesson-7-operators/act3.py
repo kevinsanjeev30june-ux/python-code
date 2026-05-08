63f7410e009e559e943ca32ee190ebc38b5b2717
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-08 13:52:17 +0530

    updates

---
+++ lesson-7-operators/act3.py
@@ -0,0 +1,16 @@
+# checking in strings
+
+sentence = 'Hello world'
+print('Hello' in sentence)
+print('world' not in sentence)
+print('python'in sentence)
+
+#checking in a list 
+a = [1,2,3,4,5]
+b = [1,2,4,5,6]
+c = a
+
+print(a in c)
+print(a not in b)
+print(2 in a)
+print([1,2,4,5,6] not in c)

b94e1dadd806f039abdd0754f5911ae7f47a6b88
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-15 13:18:31 +0530

    updates

---
+++ lesson-8-challenges/act1.py
@@ -0,0 +1,15 @@
+v = 4
+w = 6
+x = 7
+y = 9
+z = 3
+z = (v+w) * x / y
+print("Value of (v+w) * x /yis",z)
+
+name = "Kevin"
+age = 0
+if name == "Kevin" or name == "John" and age >=2 :
+    print("Hello! WELCOME>")
+
+else:
+    print("Good Night!!")

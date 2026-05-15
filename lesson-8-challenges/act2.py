cceb1eb42474bdb14514f61afb25506a1bd4f0ca
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-15 13:33:37 +0530

    updates

---
+++ lesson-8-challenges/act2.py
@@ -0,0 +1,17 @@
+mean = 38
+wrong_number = 47
+correct_number = 67
+total_nums = 40
+
+sum = mean * total_nums
+print(f"The sum of 40 numbers :{sum}")
+
+diff = correct_number - wrong_number
+
+# Correct Sum
+sum = sum + diff
+
+#Correct Mean
+correct_mean = sum/total_nums
+
+print(f"The correct mean is : {correct_mean}")

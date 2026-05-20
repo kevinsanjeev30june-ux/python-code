a7d18a8f6addff219ef2746fdbacc0dd2eeb9c82
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-20 13:28:31 +0530

    updates

---
+++ lesson-10-loops/act1.py
@@ -0,0 +1,7 @@
+# Calculate the total n of the numbers
+limit = int(input("Enter the number you want to find:"))
+total = 0
+
+for i in range(1,limit+ 1):
+    total = total + i
+print("\nTotal :", total)    

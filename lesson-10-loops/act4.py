e7bfd687f2137416426d01e2c29cf13cc4d362b1
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-21 13:12:33 +0530

    updates

---
+++ lesson-10-loops/act4.py
@@ -0,0 +1,12 @@
+# Sum of n natural numbers
+limit = int(input("Enter a limit:"))
+total = 0
+
+# while loop starts here
+i = 1
+while i<= limit:
+    total = total + i
+    i = i + 1
+
+print("\nTotal:",total)
+    

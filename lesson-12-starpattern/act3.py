ac20ae9b8314ba1cacecb2a9ac30a581a2f0758a
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-05-25 13:51:26 +0530

    updates

---
+++ lesson-12-starpattern/act3.py
@@ -0,0 +1,31 @@
+# Take input from the user
+rowsize = int(input("Enter the number of rows:"))
+if rowsize%2==0: #Conditions
+    halfDiamRow = int(rowsize/2)
+else:
+    halfDiamRow = int(rowsize/2) + 1
+space = halfDiamRow - 1
+# loop for upper part
+for i in range(1, halfDiamRow + 1): # loop for rows
+    for j in range(1,space + 1): # loop for columns
+        print(end=" ")
+
+    space = space- 1
+    num = 1
+    for j in range(2*i - 1):
+        print(end = str(num))
+    # Incrementing number at each column
+        num = num + 1
+    print()
+space = 1
+#loop for lower part
+for i in range( 1, halfDiamRow): # loop for rows
+    for j in  range(1, space + 1): # loop for columns
+        print(end = " ")
+    space = space+ 1    
+    num = 1
+    for j in range(1,2*(halfDiamRow - i)):
+        print(end = str(num)) # display the result
+       # incremrenting number at each column
+        num = num + 1
+    print() 

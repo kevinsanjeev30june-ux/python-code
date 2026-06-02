d98b87e2c1f6498b49379bee90969a4108dbefb3
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-06-02 13:45:15 +0530

    updates

---
+++ lesson-17-random-math/act3.py
@@ -0,0 +1,12 @@
+import math
+print('The Floor and ceiling value of 57.98 are:'+str(math.ceil(57.98)) +','+ str(math.floor(57.98)))
+
+x = 10
+y = - 15
+# Using Copysign function
+print('The value of x after copying  the sign of y is:'+str(math.copysign(x,y)))
+
+# using fabs and gcd functions
+print('The absolute value of -97 and 65 are:'+str(math.fabs(-97.25)) + ','+ str (math.fabs(65)))
+
+print('The greatest common divisor of 24 and 56 :'+str(math.gcd(24,56)))

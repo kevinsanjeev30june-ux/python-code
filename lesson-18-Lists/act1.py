f8d1d4adf0b08fa769330e326f4366945f0e6108
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-06-04 13:19:19 +0530

    updates

---
+++ lesson-18-Lists/act1.py
@@ -0,0 +1,15 @@
+empty_list = []
+print(empty_list)   
+
+numbers = [1, 2, 3, 4, 5,6,7]
+print(numbers)
+
+triples = numbers* 3
+print(triples)
+
+# Reverse the list
+my_list = ["apple", "banana", "cherry"]
+print(my_list)
+
+my_list.reverse()
+print(my_list)

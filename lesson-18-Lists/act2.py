4b3b5b118b8405f4c88a0b128727cc94a8b77c44
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-06-04 13:40:27 +0530

    updates

---
+++ lesson-18-Lists/act2.py
@@ -0,0 +1,53 @@
+# List methods
+
+list = [11,23,5,67,9]
+print("Before sorting..")
+print(list)
+print("After sorting..")
+print("Ascending order..")
+list.sort()
+print(list) 
+
+print("Descending order..")
+list.sort(reverse=True)
+print(list)
+
+# Reverse the list
+list.reverse()
+print("After reversing..")
+print(list)
+
+# append/add
+list.append(500)
+print(list)
+
+# Remove() a specific item from the list
+list.remove(67)
+print(list)
+
+# Remove the last item from the list
+list.pop()
+print(list)
+
+# inset an new item at a specific position
+list.insert(2, 23)
+print(list)
+
+
+# find sum
+sum = 0
+for item in list:
+    sum += item 
+print(f"Sum of the list is {sum}") 
+
+# find the average
+list_length = len(list)
+average = sum / list_length
+print(average)
+
+# Find min and max
+my_list2 = [11,2,78, 999, -5, 7,26]
+my_list2.sort()
+
+print(f"minimum : {my_list2[0]}")
+print(f"maximum : {my_list2[-1]}")

76cfbe2ed5a56aa9e60ed6c77765961c17d04523
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-06-05 17:31:39 +0530

    updates

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,11 @@
+# Create a list of fruits
+fruits = ["apple", "banana", "kiwi", "kiwi", "banana", "pineapple"]
 
+fruit_count = {}
+# Counnt the occurences of each fruit
+for fruit in fruits:
+    fruit_count[fruit] =fruits.count(fruit)
+    
+
+# print the counts of each fruit
+print(fruit_count)

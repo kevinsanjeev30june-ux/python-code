bfc7d84f23f7af4f2d6001e484993f1f4d2f8a2a
kevinsanjeev30june-ux <kevinsanjeev30june@gmail.com>
2026-06-17 17:45:01 +0530

    updates

---
+++ lesson-25-inheritance/act2.py
@@ -0,0 +1,20 @@
+# Inheritance in oop
+class Grandpa:
+    def __init__(self,name,money,car):
+        self.name = name
+        self.car = car
+        self.money = money
+
+    def displayAsset(self):
+        return f"{self.name} has ${self.money} and also a {self.car} car"
+
+class Grandson(Grandpa):
+    def __init__(self,name,money,car,laptop):
+        Grandpa.__init__(self,name,money,car)
+        self.laptop = laptop
+
+    def displayAsset(self):
+          return f"{Grandpa.displayAsset(self)}, and {self.laptop} laptop"
+    
+gs1 = Grandson("Kevin","25,00,000" ,"Rolls Royce Phantom", "lenovo slim 3")
+print(gs1.displayAsset())    
